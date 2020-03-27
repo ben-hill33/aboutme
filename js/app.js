9c7fa6dfe86d964e174fad89853131d4fdde3f51
BEN <benjamin.hill28@gmail.com>
2020-03-26 21:04:44 -0700

    added variables and if..else statements to app.js

--- js/app.js
+++ js/app.js
@@ -2,20 +2,16 @@
 
 var userName = prompt('What is your name?');
 console.log(userName + ' this is the answer to what is your name');
+alert('It\'s a pleasure to meet you ' + userName);
 
-var likeStarWars = prompt('Do you like Star Wars?');
+var codeQuestion = prompt('Do you know how to use Javascript?').toLowerCase();
+console.log('This is the answer to do you know how to use Javascript ' + codeQuestion);
 
-// if the user answers yes, Yes, yeS, y, I want to alert
-// user name and the phrase I like star wars too.
-
-//if the user answers no I want to alert
-//user name and the phrase why?
-
-if(likeStarWars === 'yes'){
-  alert(userName + ' I like Star wars too!');
+if(codeQuestion === 'yes'){
+  alert(userName + ' Will you teach me?');
 }
-else if(likeStarWars === 'no'){
-  alert(userName + ' Why don\'t you like Star Wars? Shame ');
+else if(codeQuestion === 'no'){
+  alert(userName + ' I don\'t either, but we\'ll be masters soon enough.');
 } else {
-  alert(userName + ' You did not read the instructions very well');
+  alert(userName + ' Ah, ah, ah, you didn\'t say the magic word!');
 }
